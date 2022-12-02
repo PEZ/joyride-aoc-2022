12e4a29b7b650e0b9c8bb9b40da15f16da6ecf2e
Peter Strömberg <pez@pezius.com>
2022-12-02 12:15:05 +0100

    Add status indicator answer checker

--- .joyride/scripts/aoc2022_1.cljs
+++ .joyride/scripts/aoc2022_1.cljs
@@ -1,18 +1,26 @@
 (ns aoc2022-1
   (:require [util-aoc :as aoc]))
 
-;; Start by loading `util_aoc.cljs` in the REPL
-;; (ctrl+alt+c enter)
-;; Then load this file
-;; Then evaluate the top level forms in the
-;; Rich comment form `(comment ...)` below
+;; ## How to use this template
+;; 0. Install the extensions Joyride and Calva
+;; 1. Start and connect the Joyride nREPL server
+;;    * Calva: Start Joyride REPL and Connect
+;; 2. Open the file `util_aoc.cljs` and load it in the REPL
+;;    * Calva: Load/Evaluate File (ctrl+alt+c enter)
+;; 3. Then also load this file in the REPL
+;; 4  Then evaluate the top level forms in the Rich Comment form
+;;    (comment ...) below, in the order they appear
 
 (comment
+  ;; Initialize this AOC day, creates a statusbar item
   (aoc/set-day! 1)
+  ;; You can click the item in to open the AOC site on day 1
   
-  ;; See `util_aoc.cljs` for caveats re this `def`
+  ;; Fetch your Day 1 input (If you haven't, first see README
+  ;; for how to get your browser session here)
   (def real-input (aoc/fetched-input))
 
+  ;; Give yourself some test input (copied from AOC Day 1)
   (def test-input [1000
                    2000
                    3000
@@ -43,11 +51,9 @@
          (apply +)))
 
   (part-1 test-input)
-  (part-1 real-input)
-  (aoc/inform! (part-1 real-input))
+  (aoc/update-indicator! "1" (part-1 test-input))
+  (aoc/update-indicator! "1" (part-1 real-input))
   (part-2 test-input)
-  (part-2 real-input)
-  (aoc/inform! (part-1 real-input))
+  (aoc/update-indicator! "2" (part-1 test-input))
+  (aoc/update-indicator! "2" (part-1 real-input))
   :rcf)
-
-
--- .joyride/scripts/util_aoc.cljs
+++ .joyride/scripts/util_aoc.cljs
@@ -1,13 +1,14 @@
 (ns util-aoc
   (:require ["vscode" :as vscode]
             ["requestify" :as fetch]
+            ["open" :as open]
             [clojure.edn :as edn]
             [promesa.core :as p]
             [clojure.string :as string]))
 
 (defonce !state (atom {:day nil
                        :input nil
-                       :statusbar-item nil}))
+                       :indicator nil}))
 
 (defn fetched-input []
   (:input @!state))
@@ -20,26 +21,35 @@
   `(.then (do ~@body) #(def ~name %)))
 
 (defn slurp-ws-file+ [ws-file]
-  (->
-   (p/let [ws-root (-> vscode/workspace.workspaceFolders first .-uri)
-           uri (vscode/Uri.joinPath ws-root ws-file)
-           data (vscode/workspace.fs.readFile uri)
-           text (.decode (js/TextDecoder. "utf-8") data)]
-     text)
-   (p/catch (fn [e]
-              (println "Ho, ho, ho! File not found:" ws-file e)
-              (throw (js/Error. e))))))
+  (-> (p/let [ws-root (-> vscode/workspace.workspaceFolders first .-uri)
+              uri (vscode/Uri.joinPath ws-root ws-file)
+              data (vscode/workspace.fs.readFile uri)
+              text (.decode (js/TextDecoder. "utf-8") data)]
+        text)
+      (p/catch (fn [e]
+                 (println "Ho, ho, ho! File not found:" ws-file e)
+                 (throw (js/Error. e))))))
+
+#_{:clj-kondo/ignore [:unused-private-var]}
+(defn- open-aoc-day! [day answer]
+  (if answer
+    (p/let [choice (vscode/window.showInformationMessage
+                    (str "Ho, ho, ho! Copy " answer " to the clipboard and open day " day " on adventofcode.com?")
+                    "Yes, please")]
+      (when choice
+        (-> (vscode/env.clipboard.writeText (str answer))
+            (.then #(open (str "https://adventofcode.com/2022/day/" day))))))
+    (open (str "https://adventofcode.com/2022/day/" day))))
 
 (defn- fetch-input'+ [day]
-  (->
-    (p/let [cookie (slurp-ws-file+ ".aoc-session")
-            response (.get requestify
-                           (str "https://adventofcode.com/2022/day/" day "/input")
-                           #js {:cookies #js {:session cookie}})]
-      (.getBody response))
-    (p/catch (fn [e]
-               (println "Ho, ho, ho! Did you forget to populate `.aoc-session` with your AOC session cookie?" e)
-               (throw (js/Error. e))))))
+  (-> (p/let [cookie (slurp-ws-file+ ".aoc-session")
+              response (.get requestify
+                             (str "https://adventofcode.com/2022/day/" day "/input")
+                             #js {:cookies #js {:session cookie}})]
+        (.getBody response))
+      (p/catch (fn [e]
+                 (println "Ho, ho, ho! Did you forget to populate `.aoc-session` with your AOC session cookie?" e)
+                 (throw (js/Error. e))))))
 
 (def fetch-input+
   (memoize fetch-input'+))
@@ -62,12 +72,46 @@
          (string/split-lines)
          (map read-string-or-string))))
 
+(defn update-indicator!
+  [part answer]
+  (let [{:keys [day indicator]} @!state]
+    (set! (.-text indicator) (str "Day " day ", part " part ": " answer))
+    (set! (.-tooltip indicator)
+          (str "Advent of Code 2022, day " day ", part " part ". "
+               "Want to check if " answer " is the correct answer for your input?"))
+    (set! (.-command indicator)
+          #js {:command "joyride.runCode"
+               :arguments #js [(str "(util-aoc/open-aoc-day! " 
+                                    day " " answer")")]})
+    answer))
+
+(defn create-indicator! [day]
+  (when-let [existing-indicator (:indicator @!state)]
+    (.dispose existing-indicator)
+    (swap! !state assoc :indicator nil))
+  (p/let [indicator (vscode/window.createStatusBarItem
+                vscode/StatusBarAlignment.Left
+                -1000)]
+    (def day day)
+    (def indicator indicator)
+    (set! (.-text indicator) (str "Day " day ": <go-for-it>"))
+    (set! (.-tooltip indicator) (str "Advent of Code 2022, day, " day ". Waiting for you to test your solution..."))
+    (set! (.-command indicator)
+          #js {:command "joyride.runCode"
+               :arguments #js [(str "(util-aoc/open-aoc-day! " day ")")]})
+    (.show indicator)
+    indicator))
+
 (defn set-day!
   [d]
-  (p/let [input (values+ d)]
+  (p/let [input (values+ d)
+          indicator (create-indicator! d)]
+    (def xindicator indicator)
+    (println "setting day" d "!")
     (swap! !state assoc
            :day d
-           :input input)))
+           :input input
+           :indicator indicator)))
 
 (defn inform!
   "Show an information message with `x`"
@@ -79,5 +123,7 @@
   (values+ 1)
   (fetch-input+ 1)
   (fetch-input'+ 1)
+  (set-day! 1)
+  (create-indicator! 1)
   :rcf)
 
